7221bbf58e45be6b4a2eebb1e8aacf0b2f216960
mehuliate <darksideofbotix@gmail.com>
2017-01-19 10:59:28 +0700

    update data

--- views/index.view.php
+++ views/index.view.php
@@ -1,14 +1,7 @@
 <?php require 'partials/head.php'; ?>
 	<h1>My Task</h1>
-	<ul>
-		<?php foreach ($tasks as $task): ?>
-			<li>
-			<?php if($task->completed): ?>
-				<strike><?= $task->description; ?></strike>
-			<?php else: ?>
-				<?= $task->description; ?>
-			<?php endif; ?>
-			</li>
-		<?php endforeach; ?>
-	</ul>
+	<form method="GET" action="/names">
+		<input type="text" name="name">
+		<button type="submit">Submit</button>
+	</form>
 <?php require 'partials/footer.php'; ?>
